637f722531833420548016a40797d5adc7dad38d
dlieviant <d.lieviant@gmail.com>
2014-10-28 22:49:49 +0000

    added files to run program

--- Bible.py
+++ Bible.py
@@ -1,31 +1,16 @@
 import re
 import yaml
 import os
-import "../speaker"
+import speaker
 import urllib2
 import mpd
-from "../mic" import Mic
+from mic import Mic
 import time
 import bible_search
-import getch
 import sys
 import select
 
-#WORDS = ["READ","FIRST", "SECOND", "THIRD", "BIBLE"
-#	 "GENESIS", "EXODUS", "LEVITICUS", "NUMBERS", "DEUTERONOMY", "JOSHUA",
-#	 "JUDGES", "RUTH", "SAMUEL", "KINGS", "CHRONICLES", "EZRA", "NEHEMIAH",
-#	 "ESTHER", "JOB", "PSALMS", "PROVERBS", "ECCLESIASTES", "SONG",
-#	 "SOLOMON", "ISAIAH", "JEREMIAH", "LAMENTATIONS", "EZEKIEL", "DANIEL",
-#	 "HOSEA", "JOEL", "AMOS", "OBADIAH", "JONAH", "MICAH", "NAHUM",
-#	 "HABAKKUK", "ZEPHANIAH", "HAGGAI", "ZECHARIAH", "MALACHI",
-#	 "MATTHEW", "MARK", "LUKE", "JOHN", "ACTS", "ROMANS", "CORINTHIANS", 
-#	 "GALATIANS", "EPHESIANS", "PHILIPPIANS", "COLOSSIANS", "THESSALONIANS",
-#	 "TIMOTHY", "TITUS", "PHILEMON", "HEBREWS", "JAMES", "PETER", "JOHN",
-#	 "JUDE", "REVELATIONS"]
-
 WORDS = ["READ", "BIBLE"]
-
-test_uri = "http://cloud.faithcomesbyhearing.com/mp3audiobibles2/ENGESVO2DA/A01___01_Genesis_____ENGESVO2DA.mp3"
 
 profile = yaml.safe_load(open("profile.yml", "r"))
 mic = Mic(speaker.newSpeaker(), "languagemodel_bible.lm", "dictionary_bible.dic", "languagemodel_persona.lm", "dictionary_persona.dic", lmd_music="languagemodel_playback.lm", dictd_music="dictionary_playback.dic", lmd_num="languagemodel_num.lm", dictd_num="dictionary_num.dic")
@@ -39,8 +24,6 @@ def isValid(text):
         Arguments:
         text -- user-input, typically transcribed speech
     """
-    #return bool(re.search(r'\b(genesis|exodus|leviticus|numbers|deuteronomy|joshua|judges|ruth|samuel|kings|chronicles|ezra|nehemiah|esther|job|psalms|proverbs|ecclesiates|song of solomon|isaiah|jeremiah|lamentations|ezekiel|daniel|hosea|joel|amos|obadiah|jonah|micah|nahum|habakkuk|zephaniah|haggai|zechariah|malachi|matthew|mark|luke|john|acts|romans|corinthians|galatians|ephesians|philippians|colossians|thessalonians|timothy|titus|philemon|hebrews|james|john|jude|revelations)\b', text, re.IGNORECASE))
-
     return bool(re.search(r'\b(read|bible)\b', text, re.IGNORECASE))
 
   
@@ -92,23 +75,20 @@ class BibleReader:
             '''
             inputFlag = False
             try:
-		#char = getch.getch()
                 print "key input"
                 i, o, e = select.select([sys.stdin], [], [], 0)
                 for s in i:
                     if s == sys.stdin:
                         input = sys.stdin.read(1)
                         inputFlag = True
-                print "voice input"
-                threshold, transcribed = self.mic.passiveListen(self.persona)
+                if not inputFlag:
+                    print "voice input"
+                    threshold, transcribed = self.mic.passiveListen(self.persona)
             except:
                 continue
 
-            #if threshold or char == 'j':
             if threshold or inputFlag:
                 inputFlag = False
-		#if char == 'j':
-                #    print "manual persona activation"
                 try:
                     self.client.pause(1)
                 except mpd.ConnectionError:
